b120f09db931c091a82620363c9c8c6bf7a3fc59
Dimas Aulia Fachrudin <dimasauliafachrudin@gmail.com>
2025-01-19 11:24:34 +0700

    Server first API

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"finance/provider/configuration"
+	"finance/provider/http"
+	"finance/route"
+)
 
 func main() {
-	fmt.Println("Finance System")
+	// Get ENV File
+	env := configuration.NewConfiguration(".env").LoadEnv()
+	port := env.GetString("SERVER_PORT")
+	fork := env.GetBool("SERVER_FORK")
+
+	// Create Server Instance
+	server := http.NewHttpServer(port, fork)
+	app := server.Setup()
+
+	// Setup Router
+	route.NewRoute(app).SetupMainRouter()
+
+	server.Start(app)
+
 }
