ce9b5091f780d373ed1a46d16f699ba4241b7ddd
Dimas Aulia Fachrudin <dimasauliafachrudin@gmail.com>
2025-01-19 19:09:19 +0700

    Setup Logger

--- provider/http/http.provider.go
+++ provider/http/http.provider.go
@@ -2,9 +2,13 @@ package http
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
 
 type HttpServerImpl struct {
@@ -25,6 +29,15 @@ func (h *HttpServerImpl) Setup() *fiber.App {
 	}
 
 	app := fiber.New(appConfig)
+
+	app.Use(requestid.New())
+	app.Use(logger.New(logger.Config{
+		// For more options, see the Config section
+		Format:     "${time} ${ip}:${port} ${status} - ${method} ${path}\n",
+		TimeZone:   "Asia/Jakarta",
+		TimeFormat: "02/01/2006 15:04:05.000000",
+		Output:     io.Writer(os.Stdout),
+	}))
 
 	return app
 }
