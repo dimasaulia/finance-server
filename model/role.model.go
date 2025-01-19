fbae57e8c3a84c46e166b77ee048f8a9a2e4eb59
Dimas Aulia Fachrudin <dimasauliafachrudin@gmail.com>
2025-01-19 19:56:08 +0700

    Change column name

--- model/role.model.go
+++ model/role.model.go
@@ -5,7 +5,7 @@ import "time"
 const TB_ROLE string = `"ROLE"`
 
 type Role struct {
-	Role_Id   string    `gorm:"primaryKey"`
+	Id_Role   string    `gorm:"primaryKey"`
 	Name      string    `gorm:"unique; not null"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime"`
